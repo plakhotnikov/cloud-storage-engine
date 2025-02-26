1511a36e214f0dde02a9cf3c8d26371433560365
Vova <not.your.boy@yandex.ru>
2025-02-26 21:22:27 +0300

    spring actuator + prometheus + grafana with 2 dashboards added

--- src/main/java/com/plakhotnikov/cloud_storage_engine/config/SecurityConfig.java
+++ src/main/java/com/plakhotnikov/cloud_storage_engine/config/SecurityConfig.java
@@ -23,7 +23,8 @@ public class SecurityConfig {
     private static final String[] WHITE_LIST_URLS = {
             "/auth/**",
             "/swagger-ui/**",
-            "/v3/api-docs/**"
+            "/v3/api-docs/**",
+            "/actuator/**"
     };
 
 
